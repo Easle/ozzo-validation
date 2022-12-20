2b3e676a8de05c427739a7d0e0e80207ee07c46b
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-12-20 15:28:29 +0400

    change import path + upgrade to go 1.19

--- example_test.go
+++ example_test.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"regexp"
 
-	"github.com/go-ozzo/ozzo-validation/v4"
-	"github.com/go-ozzo/ozzo-validation/v4/is"
+	"github.com/easle/ozzo-validation/v5"
+	"github.com/easle/ozzo-validation/v5/is"
 )
 
 type Address struct {
--- is/rules_test.go
+++ is/rules_test.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/easle/ozzo-validation/v5"
 	"github.com/stretchr/testify/assert"
 )
 
